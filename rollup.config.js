dd054ad7ae23e25d8e3f3c2d9e3799d17bc88570
Callum Macrae <callum@macr.ae>
2016-02-14 19:05:48 +0000

    Fixed rollup!

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,17 @@
-//import uglify from 'rollup-plugin-uglify';
+import uglify from 'rollup-plugin-uglify';
+import babel from 'rollup-plugin-babel';
 
 export default {
 	entry: 'src/index.js',
 	dest: 'dist/deity.min.js',
 	sourceMap: true,
-	//plugins: [ uglify() ],
+	plugins: [
+		babel({
+			presets: ['es2015-rollup'],
+			babelrc: false
+		}),
+		uglify()
+	],
 	format: 'umd',
 	moduleName: 'deity'
 };
